dbe039d5ad792d48a39f9f9306dab0050ecdea5f
Derek <derekcorwin@gmail.com>
2020-12-16 10:56:05 -0600

    2

---
+++ www.cimcoinc.net/index7b17.php
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! 1.5 - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Home</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://www.cimcoinc.net"/>
+	<id>http://www.cimcoinc.net/index.php</id>
+	<updated>2018-03-28T12:30:42Z</updated>
+	<generator uri="http://joomla.org" version="1.5">Joomla! 1.5 - Open Source Content Management</generator>
+<link rel="self" type="application/atom+xml" href="http://www.cimcoinc.net/index.php?format=feed&amp;type=atom" />
+	<entry>
+		<title>Welcome</title>
+		<link rel="alternate" type="text/html" href="http://www.cimcoinc.net/index.php/component/content/article/2-welcome"/>
+		<published>2012-03-04T00:24:24Z</published>
+		<updated>2012-03-04T00:24:24Z</updated>
+		<id>http://www.cimcoinc.net/index.php/component/content/article/2-welcome</id>
+		<author>
+			<name>Karen</name>
+		<email>admin@cimcoinc.net</email>
+		</author>
+		<summary type="html">&lt;p&gt;&lt;span style=&quot;text-align: left;&quot;&gt;&lt;img class=&quot;caption&quot; src=&quot;http://www.cimcoinc.net/images/stories/fullscreen capture 332012 92029 pm.bmp.jpg&quot; border=&quot;0&quot; alt=&quot;Underground Conduit&quot; title=&quot;Underground Conduit&quot; align=&quot;left&quot; /&gt;Cimco, Inc. is a licensed Arkansas General Contractor based in the central Arkansas area. Cimco was established in 1975 and has been serving clients for over 37 years.  Cimco, Inc. can provide everything from site preparation, concrete slab and foundation work and metal buildings to highly technical equipment relocation and installation.  We can be a single source for turnkey projects.  Cimco, Inc. has a dependable fleet of equipment and a skilled workforce to self perform work and also a strong base of qualified specialty subcontractors to handle any of your needs.  Cimco, Inc. can price your project as a hard bid, a not to exceed price or a negotiated price.  We can perform your project on a time and material basis that is competitive with any comparative contractor.&lt;/span&gt;&lt;/p&gt;</summary>
+		<content type="html">&lt;p&gt;&lt;span style=&quot;text-align: left;&quot;&gt;&lt;img class=&quot;caption&quot; src=&quot;http://www.cimcoinc.net/images/stories/fullscreen capture 332012 92029 pm.bmp.jpg&quot; border=&quot;0&quot; alt=&quot;Underground Conduit&quot; title=&quot;Underground Conduit&quot; align=&quot;left&quot; /&gt;Cimco, Inc. is a licensed Arkansas General Contractor based in the central Arkansas area. Cimco was established in 1975 and has been serving clients for over 37 years.  Cimco, Inc. can provide everything from site preparation, concrete slab and foundation work and metal buildings to highly technical equipment relocation and installation.  We can be a single source for turnkey projects.  Cimco, Inc. has a dependable fleet of equipment and a skilled workforce to self perform work and also a strong base of qualified specialty subcontractors to handle any of your needs.  Cimco, Inc. can price your project as a hard bid, a not to exceed price or a negotiated price.  We can perform your project on a time and material basis that is competitive with any comparative contractor.&lt;/span&gt;&lt;/p&gt;</content>
+	</entry>
+</feed>
---
+++ www.cimcoinc.net/indexc0d0.php
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! 1.5 - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Home</title>
+		<description></description>
+		<link>http://www.cimcoinc.net/index.php/component/content/frontpage</link>
+		<lastBuildDate>Wed, 28 Mar 2018 12:30:42 +0000</lastBuildDate>
+		<generator>Joomla! 1.5 - Open Source Content Management</generator>
+		<language>en-gb</language>
+		<item>
+			<title>Welcome</title>
+			<link>http://www.cimcoinc.net/index.php/component/content/article/2-welcome</link>
+			<guid>http://www.cimcoinc.net/index.php/component/content/article/2-welcome</guid>
+			<description><![CDATA[<p><span style="text-align: left;"><img class="caption" src="http://www.cimcoinc.net/images/stories/fullscreen capture 332012 92029 pm.bmp.jpg" border="0" alt="Underground Conduit" title="Underground Conduit" align="left" />Cimco, Inc. is a licensed Arkansas General Contractor based in the central Arkansas area. Cimco was established in 1975 and has been serving clients for over 37 years.  Cimco, Inc. can provide everything from site preparation, concrete slab and foundation work and metal buildings to highly technical equipment relocation and installation.  We can be a single source for turnkey projects.  Cimco, Inc. has a dependable fleet of equipment and a skilled workforce to self perform work and also a strong base of qualified specialty subcontractors to handle any of your needs.  Cimco, Inc. can price your project as a hard bid, a not to exceed price or a negotiated price.  We can perform your project on a time and material basis that is competitive with any comparative contractor.</span></p>]]></description>
+			<author>admin@cimcoinc.net (Karen)</author>
+			<category>frontpage</category>
+			<pubDate>Sun, 04 Mar 2012 00:24:24 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
